5df8e017d40b5d0b3f2b414ac7e662100f723900
cnviagem-hue <cnviagem@gmail.com>
2026-09-02 22:30:59 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1747,6 +1747,5 @@ function controlarCamposConfiguracao(bloquear) {
     if(document.getElementById("btnGpsConfigs")) document.getElementById("btnGpsConfigs").disabled = bloquear;
     if(document.getElementById("btnBuscarCep")) document.getElementById("btnBuscarCep").disabled = bloquear;
 } // Fecha a função controlarCamposConfiguracao
+});
 
-} // Fecha alguma função anterior que ficou aberta
-}); // Fecha o escopo principal da página
